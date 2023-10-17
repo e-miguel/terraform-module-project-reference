03f6c24008565f10a0a02746e8a2ad1e348b43c4
Miguel <eugeniomiguelmendoza@gmail.com>
2023-10-17 17:33:51 +0100

    Update alb reference.tf

--- alb reference.tf
+++ alb reference.tf
@@ -1,71 +1,60 @@
 # create application load balancer
-# terraform aws create application load balancer
 resource "aws_lb" "application_load_balancer" {
-  name               = 
+  name               = "${}-alb"
   internal           = 
   load_balancer_type = 
-  security_groups    = 
-
-  subnet_mapping {
-    subnet_id = 
-  }
-
-  subnet_mapping {
-    subnet_id = 
-  }
-
-  enable_deletion_protection = false
+  security_groups    = []
+  subnets            = []
+  enable_deletion_protection = 
 
   tags   = {
-    Name = 
+    Name = "${}-alb"
   }
 }
 
 # create target group
-# terraform aws create target group
 resource "aws_lb_target_group" "alb_target_group" {
-  name        = 
+  name        = "${}-tg"
   target_type = 
   port        = 
   protocol    = 
   vpc_id      = 
 
   health_check {
-    healthy_threshold   = 
-    interval            = 
-    matcher             = 
-    path                = 
-    port                = 
-    protocol            = 
-    timeout             = 
-    unhealthy_threshold = 
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = 200
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
+  }
+
+  lifecycle {
+    create_before_destroy = 
   }
 }
 
 # create a listener on port 80 with redirect action
-# terraform aws create listener
 resource "aws_lb_listener" "alb_http_listener" {
-  load_balancer_arn = aws_lb.application_load_balancer.arn
+  load_balancer_arn = 
   port              = 
   protocol          = 
 
   default_action {
-    type = 
+    type = "redirect"
 
     redirect {
-      host        = 
-      path        = 
-      port        = 
-      protocol    = 
-      status_code = 
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
 
 # create a listener on port 443 with forward action
-# terraform aws create listener
 resource "aws_lb_listener" "alb_https_listener" {
-  load_balancer_arn  = aws_lb.application_load_balancer.arn
+  load_balancer_arn  = 
   port               = 
   protocol           = 
   ssl_policy         = "ELBSecurityPolicy-2016-08"
